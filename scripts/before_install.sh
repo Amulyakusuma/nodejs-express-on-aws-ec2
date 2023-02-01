4d3e74580d2a3fbb3d51de573ce7358066ddb606
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-01 17:44:46 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,7 +5,7 @@ curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 apt-get install -y nodejs
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
+DIR="/var/www/html/server"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
