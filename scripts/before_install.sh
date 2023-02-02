d9ad0b246e8308dd9d78f8408ff9b6f4c00fd063
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-02 16:20:29 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#download node and npm
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt-get install -y nodejs
-
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/test"
 if [ -d "$DIR" ]; then
