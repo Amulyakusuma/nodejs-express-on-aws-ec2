6b7922f8318d5d5161ab3447db17d0b42966467b
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-02 16:45:50 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
 
 #create our working directory if it doesnt exist
-DIR="/home/ubuntu/test"
+DIR="/var/www/html/application"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
+
+APP_DIR=/var/www/html/application
+HOSTNAME=__SERVER_HOSTNAME__
+APP_VERSION=__VERSION__
+ENV=__ENV__
+
+#########################################
+######### LARAVEL CONFIGURATION #########
+#########################################
+
+chmod -R 777 $APP_DIR/storage
+chmod -R 777 $APP_DIR/bootstrap/cache
+
+#########################################
+############### Build ENV ###############
+#########################################
+
+cp $APP_DIR/.github/deployment/server/.env $APP_DIR/.env
+sed -i "s/__VERSION__/$VERSION/g" $APP_DIR/.env
+sh $APP_DIR/.github/deployment/server/AwsSecrets.sh $ENV/$HOSTNAME $APP_DIR/.env
+
+cd $APP_DIR && php artisan optimize && php artisan config:clear
+
+#cd $APP_DIR && php artisan config:clear | fails if this is enabled
+cd $APP_DIR && php artisan route:cache
+cd $APP_DIR && php artisan view:cache
+
+##### Migration
+cd $APP_DIR && php artisan migrate --force
+
+#########################################
+############# NGNIX SERVER ##############
+#########################################
+rm -rf /etc/nginx/sites-available/default
+rm -rf /etc/nginx/sites-enabled/default
+cp $APP_DIR/.github/deployment/server/laravel.conf /etc/nginx/conf.d/laravel.conf
