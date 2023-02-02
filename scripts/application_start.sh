6a76c221583b3def212e7c70742dc7475275204d
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-02 16:34:45 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ubuntu/test
-
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/test
 
