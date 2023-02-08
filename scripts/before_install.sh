0f42c96b1456a7a02933ed624955956749285c8b
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-08 14:58:11 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-#download node and npm
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
- apt-get install -y nodejs
-
 #create our working directory if it doesnt exist
-DIR="/home/ubuntu/server"
+DIR="/var/www/html/server"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
